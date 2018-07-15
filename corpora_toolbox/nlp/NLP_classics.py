23727e8ca53c251a962a96a83e839de9df4912ed
Laura Hernandez-Dominguez <laura.hzdz@gmail.com>
2018-07-14 23:18:54 -0400

    [doc] add documentation information

--- corpora_toolbox/nlp/NLP_classics.py
+++ corpora_toolbox/nlp/NLP_classics.py
@@ -1,9 +1,36 @@
+# -*- coding: utf-8 -*-
+
+"""
+The :mod:`corpora_toolbox.nlp.NLP_classics` implements some of the most classic functions
+used for NLP analysis.
+
+"""
+
+# Author: Laura Hernandez-Dominguez <laura.hzdz@gmail.com>, 2018
+#         Laboratoire d'ingénierie Cognitive et Sémantique (LiNCS)
+#         http://lincs.etsmtl.ca
+#         École de technologie supérieure (ÉTS)
+#
+# Free software: MIT license
+
 from corpora_toolbox.utils.io import read_file_in_string
 from math import *
 import re
 
-# Function for separating the contents of a file in a corpus into lines:
+
 def divide_file_into_lines(file_with_route):
+    """
+    This function for separates the contents of a text file into lines, removing
+    any caret returns ('\r') in the process.
+
+    Parameters
+    ----------
+    file_with_route: the name of the text file with its path
+
+    Returns
+    -------
+    lines: a list of all lines found in the file.
+    """
 
     string = read_file_in_string(file_with_route)
     lines = string.replace("\r", "").split('\n')
@@ -11,73 +38,14 @@ def divide_file_into_lines(file_with_route):
     return lines
 
 
-def remove_n_gram_repetitions(sentence, n, French=False):
-    count_reps = 0
-    new_sentence = ""
-
-    #print(sentence)
-
-    # re.split(r'\W+') separates words by apostrophe, which is not good in this case.
-    # to avoid this, we temporarily replace them by a code word: "codewordapostrophe"
-    sentence = sentence.replace("\'", "codewordapostrophe")
-
-    split_sentence = re.split(r'\W+', sentence)
-    remove = [False] * len(split_sentence)
-
-    if len(split_sentence) > n:
-
-        position = 0
-        while position < len(split_sentence) - (2*n) + 1:
-            previous_n_gram = []
-            next_n_gram = []
-            for i in range(position, position+n):
-                previous_n_gram.append(split_sentence[i])
-                next_n_gram.append(split_sentence[i+n])
-
-            if previous_n_gram == next_n_gram:
-                if French:
-                    if not (split_sentence[position + n].lower() == "vous" or \
-                            split_sentence[position + n].lower() == "nous"):
-                        for i in range(0, n):
-                            remove[position + n + i] = True
-                else:
-                    remove[position + n] = True
-
-            position += 1
-
-    tokenized_sentence = re.split(r'(\W+)', sentence)
-
-    index = 0
-    for token in tokenized_sentence:
-        if token == split_sentence[index]:
-            if not remove[index]:
-                new_sentence += token
-            else:
-                count_reps+=1
-            index +=1
-        else:
-            new_sentence += token
-
-    new_sentence = new_sentence.replace("codewordapostrophe", "\'")
-    count_reps = int(count_reps/n)
-
-    """
-    print(new_sentence)
-    if count_reps>0:
-        print("count_reps: ", count_reps)
-    """
-    return new_sentence, count_reps
-
-
-
 def __separate_sentences_for_n_grams(list_of_tags, remove_punctuation=True):
     """
     __separate_sentences_for_n_grams was created exclusively for the retrieve_n_grams function.
-    It will separate the text in sentences by using the ".".
+    It will separate (chunk) the text in sentences by using the ".".
 
     Parameters
     ----------
-    tagged_tuples: A list of tagged tuples (original word, lemma, tag) from which to extract
+    list_of_tags: A list of tagged tuples (original word, lemma, tag) from which to extract
                    the n-grams.
     remove_punctuation: If True, it removes from the returned sentences all the punctuation marks
                    so that they are not included in the n-grams.
@@ -88,11 +56,11 @@ def __separate_sentences_for_n_grams(list_of_tags, remove_punctuation=True):
     sentences: A list of sentences (lists of tagged tuples) separating with ".". The "." won't
     be included in the n-grams regardless of the value of remove_punctuation.
     """
-    sentences=[]
+    sentences = []
 
-    sentence=[]
+    sentence = []
     for tag_object in list_of_tags:
-        if tag_object.lemma==".":
+        if tag_object.lemma == ".":
             sentences.append(list(sentence))
             sentence = []
         else:
@@ -103,7 +71,7 @@ def __separate_sentences_for_n_grams(list_of_tags, remove_punctuation=True):
                 sentence.append(tag_object)
 
     # In case the list_of_tags does not end with ".":
-    if len(sentence)>0:
+    if len(sentence) > 0:
         sentences.append(list(sentence))
 
     return sentences
@@ -111,6 +79,7 @@ def __separate_sentences_for_n_grams(list_of_tags, remove_punctuation=True):
 
 def estimate_richness_stats(n_grams):
     """
+    This function estimates the most used vocabulary richness measures used in text analysis.
 
     Parameters
     ----------
@@ -119,9 +88,21 @@ def estimate_richness_stats(n_grams):
 
     Returns
     -------
-    richness_stats: a dictionary with the most common statistics for measuring  vocabulary richness.
+    richness_stats: a dictionary with the most common statistics for measuring vocabulary richness.
+                keys:
+                V1: hapax legomena
+                V2: hapax dislegomena
+                N: number of words (text size)
+                V: number of different words (vocabulary size)
+                Brunet: Brunet's index
+                Honore: Honoré's statistics
+                TTR: type token ratio
+                Sichel: Sichel's formulation
+                Yule: Yule measure
+                Entropy: entropy of the word distribution
     """
-    richness_stats ={}
+
+    richness_stats = {}
 
     richness_stats["V1"] = 0
     richness_stats["V2"] = 0
@@ -160,25 +141,23 @@ def estimate_richness_stats(n_grams):
         richness_stats["TTR"] = 0
         richness_stats["Sichel"] = 0
 
-
-    sum = 0
+    addition = 0
     for i in freqs.keys():
-        sum += (int(i)**2) * freqs[str(i)]
+        addition += (int(i)**2) * freqs[str(i)]
 
     if not richness_stats["N"] == 0:
-        richness_stats["Yule"] = (((10**4)*sum)/richness_stats["N"]) - (1/richness_stats["N"])
+        richness_stats["Yule"] = (((10**4)*addition)/richness_stats["N"]) - (1/richness_stats["N"])
     else:
         richness_stats["Yule"] = 0
 
-    sum = 0
+    addition = 0
     for n_gram in n_grams.keys():
         if not richness_stats["N"] == 0:
             p = n_grams[n_gram] / richness_stats["N"]
         else:
             p = 0
-        sum += p * log(p,2)
-    richness_stats["Entropy"] = -1 * sum
-
+        addition += p * log(p,2)
+    richness_stats["Entropy"] = -1 * addition
 
     return richness_stats
 
@@ -230,17 +209,17 @@ def retrieve_n_grams(n, list_of_tags, remove_punctuation=True, lemmas=True, only
     sentences = __separate_sentences_for_n_grams(list_of_tags,remove_punctuation)
 
     for sentence in sentences:
-        if len(sentence)>=n :
+        if len(sentence) >= n :
             for i in range(len(sentence)-n+1):
-                n_words=""
-                n_tags=""
-                for j in range(0,n):
+                n_words = ""
+                n_tags = ""
+                for j in range(0, n):
                     if lemmas:
                         n_words += sentence[i+j].lemma
                     else:
                         n_words += sentence[i + j].original
                     n_tags += sentence[i + j].tag
-                    if j<n-1:
+                    if j < n-1:
                         n_words += "_"
                         n_tags += "_"
 
@@ -254,6 +233,75 @@ def retrieve_n_grams(n, list_of_tags, remove_punctuation=True, lemmas=True, only
 
     return n_grams
 
+
+def remove_n_gram_repetitions(sentence, n, French=False):
+    """
+    When dealing with dialogs and other spontaneous texts, speakers tend to repeat n-grams during
+    their speech, due to hesitation, retracing or emphasis (E.g. "yes, yes, yes... I know"). As
+    part of the pre-processing step, it is useful to eliminate this repetitions to avoid hindering
+    the performance of automatic taggers or parsers due to this phenomenon.
+
+    Parameters
+    ----------
+    sentence: The sentence from which to remove the repetition of n-grams.
+    n: the size of the n-gram
+    French: For the specific case of French language, certain n-grams are repeated as part of the
+            regular syntax (E.g. "nous nous somme leveés tôt").
+
+    Returns
+    -------
+    new_sentence: the new sentence without the repetition of n-grams
+    count_reps: the number of n-grams that were removed from the sentece
+    """
+
+    count_reps = 0
+    new_sentence = ""
+
+    # re.split(r'\W+') separates words by apostrophe. To avoid this, we temporarily
+    # replace them by a code word: "codewordapostrophe"
+    sentence = sentence.replace("\'", "codewordapostrophe")
+
+    split_sentence = re.split(r'\W+', sentence)
+    remove = [False] * len(split_sentence)
+
+    if len(split_sentence) > n:
+
+        position = 0
+        while position < len(split_sentence) - (2*n) + 1:
+            previous_n_gram = []
+            next_n_gram = []
+            for i in range(position, position+n):
+                previous_n_gram.append(split_sentence[i])
+                next_n_gram.append(split_sentence[i+n])
+
+            if previous_n_gram == next_n_gram:
+                if French:
+                    if not (split_sentence[position + n].lower() == "vous" or \
+                            split_sentence[position + n].lower() == "nous"):
+                        for i in range(0, n):
+                            remove[position + n + i] = True
+                else:
+                    remove[position + n] = True
+
+            position += 1
+
+    tokenized_sentence = re.split(r'(\W+)', sentence)
+
+    index = 0
+    for token in tokenized_sentence:
+        if token == split_sentence[index]:
+            if not remove[index]:
+                new_sentence += token
+            else:
+                count_reps+=1
+            index +=1
+        else:
+            new_sentence += token
+
+    new_sentence = new_sentence.replace("codewordapostrophe", "\'")
+    count_reps = int(count_reps/n)
+
+    return new_sentence, count_reps
 
 
 
